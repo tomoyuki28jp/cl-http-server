10a2efd6d1b6f29294eb6a1fe3db47074ed2f19b
Tomo Matsumoto <tomoyuki28jp@yahoo.co.jp>
2009-03-14 20:37:12 +0900

    Updated package.lisp to export html

--- src/package.lisp
+++ src/package.lisp
@@ -2,7 +2,8 @@
 
 (defpackage :cl-http-server
   (:use :cl :anaphora :usocket :cl-fad :flexi-streams :cl-utilities)
-  (:export :*server*
+  (:export :html
+           :*server*
            :*request*
            :*response*
            :*http-stream*
